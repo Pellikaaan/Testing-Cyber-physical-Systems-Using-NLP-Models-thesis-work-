62e3e3d0f94df87a8c01bb8d8f724784d8766e41
Pellikaaan <98748085+Pellikaaan@users.noreply.github.com>
2025-02-13 15:52:15 +0100

    fixing gitignore

--- app/ios/Flutter/flutter_export_environment.sh
+++ app/ios/Flutter/flutter_export_environment.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Applications/Development/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/jenny/Documents/Testing-Cyber-physical-Systems-Using-NLP-Models-thesis-work-/app"
+export "FLUTTER_ROOT=/Users/pelli/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/pelli/Documents/GitHub/Testing-Cyber-physical-Systems-Using-NLP-Models-thesis-work-/app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/pelli/Documents/GitHub/Testing-Cyber-physical-Systems-Using-NLP-Models-thesis-work-/app/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/pelli/Documents/GitHub/Testing-Cyber-physical-Systems-Using-NLP-Models-thesis-work-/app/.dart_tool/package_config.json"
