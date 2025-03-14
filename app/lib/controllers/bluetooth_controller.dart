0534eb83a53c9f7c850d0049713f21a5a6d42b76
Jenny Gran <147407945+jgran1@users.noreply.github.com>
2025-03-14 16:29:54 +0100

    Updated gitignore to not ignore the Screens and Controllers.

--- app/lib/controllers/bluetooth_controller.dart
+++ app/lib/controllers/bluetooth_controller.dart
@@ -1,15 +1,20 @@
 //https://www.youtube.com/watch?v=an4NbIjcXYI
 
+import 'dart:io';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
 import 'package:get/get.dart';
 
 class BluetoothController extends GetxController {
   var isScanning = false.obs;
+  var isConnected = false.obs;
+  var connectedDeviceName = "".obs;
+  final File logFile = File("ble_logs.txt");
 
   @override
   void onInit() {
     super.onInit();
 
+/*
     FlutterBluePlus.scanResults.listen((results) {
       if (results.isEmpty) {
         print("No devices found during scan.");
@@ -20,7 +25,33 @@ class BluetoothController extends GetxController {
       }
     });
   }
+*/
 
+// Add log output to text file, for the test file to be able to read info about scanning
+    FlutterBluePlus.scanResults.listen((results) {
+      for (var result in results) {
+        String logEntry = "Device name: ${result.device.platformName}. RSSI: ${result.rssi}\n";
+        logFile.writeAsStringSync(logEntry, mode: FileMode.append);
+        print(logEntry);
+      }
+    });
+
+//Log connections and disconnections in text file for the test file to be able to read info about connections
+  FlutterBluePlus.connectedDevices.listen((devices) {
+      if (devices.isNotEmpty) {
+        isConnected.value = true;
+        connectedDeviceName.value = devices.first.platformName;
+        String logEntry = "Connected to: ${devices.first.platformName}\n";
+        logFile.writeAsStringSync(logEntry, mode: FileMode.append);
+        print(logEntry);
+      } else {
+        isConnected.value = false;
+        logFile.writeAsStringSync("Disconnected.\n", mode: FileMode.append);
+        print("Disconnected.");
+      }
+    }
+    );
+  }
 
   Future<void> scanDevices() async {
     if (isScanning.value) return;
@@ -28,11 +59,16 @@ class BluetoothController extends GetxController {
     isScanning.value = true;
 
     try {
+      logFile.writeAsStringSync("Scanning started...\n", mode: FileMode.append);
       await FlutterBluePlus.startScan(timeout: const Duration(seconds: 10), androidUsesFineLocation: true);
     } catch (e) {
+      String errorLog = "Scan error: $e\n";
+      logFile.writeAsStringSync(errorLog, mode: FileMode.append);
       print("Error during scan: $e");
     } finally {
       isScanning.value = false;
+      logFile.writeAsStringSync("Scanning finished.\n", mode: FileMode.append);
+      print("Scanning finished.");
     }
   }
 
